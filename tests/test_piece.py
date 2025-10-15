16aed17f4c184f1ef492e2414da89f0bb27c8dd7
Benjamin-svg166 <bnbussie@gmail.com>
2025-10-15 02:07:45 -0400

    Update test_piece.py

--- tests/test_piece.py
+++ tests/test_piece.py
@@ -1,7 +1,11 @@
+from engine.board_manager import BoardManager
+from engine.piece_logic_engine import PieceLogicEngine
 from engine.piece import Piece
 
-def test_piece_position():
-    p = Piece("bishop", "white", 0, 2, 3)
-    assert p.get_position() == (0, 2, 3)
-    p.set_position(1, 4, 4)
-    assert p.get_position() == (1, 4, 4)
+def test_knight_moves():
+    board = BoardManager()
+    logic = PieceLogicEngine(board)
+    knight = Piece("knight", "white", 1, 4, 4)
+    board.place_piece(knight, 1, 4, 4)
+    moves = logic.get_valid_moves(knight)
+    assert any(move[0] != 1 for move in moves), "Knight should move across layers"
